bbe4303e6721a8fcb81daef607ba88e347e58d50
Umarani <umarani.tejomurtula@microsoft.com>
2025-04-11 11:42:48 +0530

    Update CommentServiceImpl.java

--- src/main/java/com/codewithdurgesh/blog/services/impl/CommentServiceImpl.java
+++ src/main/java/com/codewithdurgesh/blog/services/impl/CommentServiceImpl.java
@@ -59,4 +59,16 @@ public class CommentServiceImpl implements CommentService {
 		this.commentRepo.delete(com);
 	}
 
+	-- SQL Statement 1
+SELECT * FROM users WHERE user_id = 123;
+
+-- SQL Statement 2
+SELECT * FROM posts WHERE post_date > '2025-01-01';
+
+-- SQL Statement 3
+SELECT * FROM comments WHERE comment_status = 'approved';
+
+-- SQL Statement 4
+SELECT * FROM orders WHERE order_status = 'completed';
+
 }
